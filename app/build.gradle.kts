7823f600efd89dbb50a650971121aefa2d410f6a
Miguel <michelnd@hotmail.com>
2024-02-24 18:13:06 +0000

    Conf. Gradle y Manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Implementación Tratamiento GSON y binding
+    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+
+    // Implementación API REST
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
 }
--- app/src/main/java/com/mnantond/dball/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mnantond/dball/ui/login/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.mnantond.dball
+package com.mnantond.dball.ui.login
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mnantond.dball.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
